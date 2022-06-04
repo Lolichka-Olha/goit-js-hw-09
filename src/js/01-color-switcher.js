eefb2833cb58efc29dabbc2e764d2baec8c28125
Olha Truba <o.ljolik@gmail.com>
2022-06-04 17:21:05 +0300

    adds task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId;
+
+function onStartBtnClick() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  //   refs.startBtn.setAttribute('disabled', true);
+  //   refs.stopBtn.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  //   refs.startBtn.removeAttribute('disabled');
+  //   refs.stopBtn.setAttribute('disabled', true);
+
+  clearInterval(intervalId);
+}
